c3fa55d39b61bb338a037fe53b294698f058b516
mohammadmahdi <shaitomohammad37@gmail.com>
2026-08-01 22:43:51 +0300

    ml module structure

--- backend/src/main/java/com/mealmate/backend/pantry/pantryRepository.java
+++ backend/src/main/java/com/mealmate/backend/pantry/pantryRepository.java
@@ -1,7 +1,6 @@
 package com.mealmate.backend.pantry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PantryRepository extends JpaRepository<PantryItem, Long> {
 }
